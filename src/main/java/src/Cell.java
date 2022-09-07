5f7cbb69ad55169ea3c09546154d4a0b6b25f25f
PixlePixle <67672793+PixlePixle@users.noreply.github.com>
2022-09-07 13:57:26 -0400

    Added ability to flag cells

--- src/main/java/src/Cell.java
+++ src/main/java/src/Cell.java
@@ -1,8 +1,11 @@
 package src;
 
 import src.Board;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 public class Cell extends Button{
     private int positionRow;
@@ -10,6 +13,7 @@ public class Cell extends Button{
     private int surroundingMines;
     private boolean mine;
     private boolean flag;
+    public boolean recursed = false;
     private static Board board;
 
     // Create a way of detecting if the button is right clicked and have it add an icon of a flag
@@ -42,6 +46,19 @@ public class Cell extends Button{
         this.setOnAction(e -> {
             action();
         });
+        this.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.SECONDARY) {
+                    flag = !flag;
+                    if (flag) {
+                        setText("🚩");
+                    } else {
+                        setText(" ");
+                    }
+                }
+            }            
+        });
     }
 
     public void action() {
@@ -57,24 +74,35 @@ public class Cell extends Button{
             } else {
                 setText(" ");
                 recurse();
+                recursed = false;
             }
 
         }
     }
 
     private void recurse() {
-        if(positionRow-1 >= 0 && !board.board[positionRow-1][positionColumn].isDisabled()) {
+        recursed = true;
+        if(positionRow-1 >= 0 && !board.board[positionRow-1][positionColumn].recursed) {
             board.board[positionRow-1][positionColumn].action();
+            if(positionColumn-1 >= 0 && !board.board[positionRow-1][positionColumn-1].recursed)
+                board.board[positionRow-1][positionColumn-1].action();
         }
-        if(positionRow+1 < board.rows && !board.board[positionRow+1][positionColumn].isDisabled()) {
+        if(positionRow+1 < board.rows && !board.board[positionRow+1][positionColumn].recursed) {
             board.board[positionRow+1][positionColumn].action();
+            if(positionColumn+1 < board.columns && !board.board[positionRow+1][positionColumn+1].recursed)
+                board.board[positionRow+1][positionColumn+1].action();
         }
-        if(positionColumn-1 >= 0 && !board.board[positionRow][positionColumn-1].isDisabled()) {
+        if(positionColumn-1 >= 0 && !board.board[positionRow][positionColumn-1].recursed) {
             board.board[positionRow][positionColumn-1].action();
+            if(positionRow+1 < board.rows && !board.board[positionRow+1][positionColumn-1].recursed)
+                board.board[positionRow+1][positionColumn-1].action();
         }
-        if(positionColumn+1 < board.columns && !board.board[positionRow][positionColumn+1].isDisabled()) {
+        if(positionColumn+1 < board.columns && !board.board[positionRow][positionColumn+1].recursed) {
             board.board[positionRow][positionColumn+1].action();
+            if(positionRow-1 >= 0 && !board.board[positionRow-1][positionColumn+1].recursed)
+                board.board[positionRow-1][positionColumn+1].action();
         }
+        
         
     }
 
